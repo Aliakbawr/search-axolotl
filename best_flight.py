e456066b7629bbb05823f0973b2b4be7cdbead3d
MehrAzin Marzough <mehrazin.marzough@gmail.com>
2023-10-21 01:45:28 +0330

    i think i managed to understand pandas

--- best_flight.py
+++ best_flight.py
@@ -1,2 +1,5 @@
-print("hello ali")
-print('Hi MehrAzin. Welcome to Axolotl!')
+#https://www.w3schools.com/python/pandas/default.asp
+import pandas as pd
+df = pd.read_csv('./Flight_Data_Easy.csv')
+#if you need to use specific column of a specific row, use this line:)
+#print(df.iloc[0,3])
