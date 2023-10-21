c0336b8f97af79c0788cf3b4056660cff682585f
Aliakbar Ahrari <ahr04624@gmail.com>
2023-10-21 15:37:02 +0330

    Graph class implemented
    For Dijkstra algorithm, we need to work on a graph-based model.

--- best_flight.py
+++ best_flight.py
@@ -1,5 +1,51 @@
 #https://www.w3schools.com/python/pandas/default.asp
+#https://www.udacity.com/blog/2021/10/implementing-dijkstras-algorithm-in-python.html
+
 import pandas as pd
-df = pd.read_csv('./Flight_Data_Easy.csv')
-#if you need to use specific column of a specific row, use this line:)
+import sys
+
+#If you need to use specific column of a specific row, use this line:)
+df = pd.read_csv('./Flight_Data.csv')
 #print(df.iloc[0,3])
+
+#Implementing the graph class
+class Graph(object):
+    def __init__(self, nodes, init_graph):
+        self.nodes = nodes
+        self.graph = self.construct_graph(nodes, init_graph)
+        
+    def construct_graph(self, nodes, init_graph):
+        '''
+        This method makes sure that the graph is symmetrical. In other words, if there's a path from node A to B with a value V, there needs to be a path from node B to node A with a value V.
+        '''
+        graph = {}
+        for node in nodes:
+            graph[node] = {}
+        
+        graph.update(init_graph)
+        
+        for node, edges in graph.items():
+            for adjacent_node, value in edges.items():
+                if graph[adjacent_node].get(node, False) == False:
+                    graph[adjacent_node][node] = value
+                    
+        return graph
+    
+    def get_nodes(self):
+        "Returns the nodes of the graph."
+        return self.nodes
+    
+    def get_outgoing_edges(self, node):
+        "Returns the neighbors of a node."
+        connections = []
+        for out_node in self.nodes:
+            if self.graph[node].get(out_node, False) != False:
+                connections.append(out_node)
+        return connections
+    
+    def value(self, node1, node2):
+        "Returns the value of an edge between two nodes."
+        return self.graph[node1][node2]
+
+
+    
