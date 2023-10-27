0f223832de365e1e1e5afe4931610a1a97a18488
Aliakbar Ahrari <ahr04624@gmail.com>
2023-10-27 23:51:28 +0330

    Update best_flight.py

--- best_flight.py
+++ best_flight.py
@@ -75,48 +75,6 @@ class Graph(object):
         "Returns the value of an edge between two nodes."
         return self.graph[node1][node2]
 
-#Dijkstra function implementation
-def dijkstra_algorithm(graph, start_node):
-    unvisited_nodes = list(graph.get_nodes())
- 
-    # We'll use this dict to save the cost of visiting each node and update it as we move along the graph   
-    shortest_path = {}
- 
-    # We'll use this dict to save the shortest known path to a node found so far
-    previous_nodes = {}
- 
-    # We'll use max_value to initialize the "infinity" value of the unvisited nodes   
-    max_value = sys.maxsize
-    for node in unvisited_nodes:
-        shortest_path[node] = max_value
-    # However, we initialize the starting node's value with 0   
-    shortest_path[start_node] = 0
-    
-    # The algorithm executes until we visit all nodes
-    while unvisited_nodes:
-        # The code block below finds the node with the lowest score
-        current_min_node = None
-        for node in unvisited_nodes: # Iterate over the nodes
-            if current_min_node == None:
-                current_min_node = node
-            elif shortest_path[node] < shortest_path[current_min_node]:
-                current_min_node = node
-                
-        # The code block below retrieves the current node's neighbors and updates their distances
-        neighbors = graph.get_outgoing_edges(current_min_node)
-        for neighbor in neighbors:
-            tentative_value = shortest_path[current_min_node] + graph.value(current_min_node, neighbor)
-            if tentative_value < shortest_path[neighbor]:
-                shortest_path[neighbor] = tentative_value
-                # We also update the best path to the current node
-                previous_nodes[neighbor] = current_min_node
-
- 
-        # After visiting its neighbors, we mark the node as "visited"
-        unvisited_nodes.remove(current_min_node)
-    
-    return previous_nodes, shortest_path
-
 
 #Modeling csv rows into lists 
 SourceAirports = df['SourceAirport'].tolist()
@@ -136,11 +94,10 @@ for n in range(0,5):
 graph = Graph(SourceAirports, init_graph)
 #execution time for Dijkstra function calculation	
 start_time = time.process_time()
-previous_nodes, shortest_path = dijkstra_algorithm(graph=graph, start_node="Dubai International Airport")
 end_time = time.process_time()
 execution_time = end_time - start_time
 minutes = int(execution_time // 60)
 seconds = int(execution_time % 60)
 print(f"Dijkstra Algorithm\nExecution Time: {minutes}m{seconds}s\n.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-")
-print_result_dijkstra(previous_nodes, shortest_path, start_node="Dubai International Airport", target_node="Imam Khomeini International Airport")
+
     
