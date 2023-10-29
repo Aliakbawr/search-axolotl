8f10223a298d176a4324620b2852a9ff72b7696f
Aliakbar Ahrari <ahr04624@gmail.com>
2023-10-29 13:32:44 +0330

    Dijkstra Update

--- best_flight.py
+++ best_flight.py
@@ -1,5 +1,6 @@
 # https://www.w3schools.com/python/pandas/default.asp
 # https://www.udacity.com/blog/2021/10/implementing-dijkstras-algorithm-in-python.html
+import sys
 
 import pandas as pd
 import networkx as nx
@@ -24,7 +25,7 @@ def a_generate_cost(param):
 
 
 def d_generated_cost(param):
-    pass
+    return 1
 
 
 def add_edge(i):
@@ -34,10 +35,50 @@ def add_edge(i):
             cost = a_generate_cost(df.iloc[i])
             G.add_edge(df.iloc[i, 1], df.iloc[i, 2], weight=cost)
     else:
-        for i in range(df.size):
-            cost = d_generated_cost(df.iloc[i])
+        for i in range(12849):
+            cost = 1
             G.add_edge(df.iloc[i, 1], df.iloc[i, 2], weight=cost)
 
+
+def dijkstra_algorithm(graph, start_node):
+    unvisited_nodes = list(graph.nodes)
+
+    # We'll use this dict to save the cost of visiting each node and update it as we move along the graph
+    shortest_path = {}
+
+    # We'll use this dict to save the shortest known path to a node found so far
+    previous_nodes = {}
+
+    # We'll use max_value to initialize the "infinity" value of the unvisited nodes
+    max_value = sys.maxsize
+    for node in unvisited_nodes:
+        shortest_path[node] = max_value
+    # However, we initialize the starting node's value with 0
+    shortest_path[start_node] = 0
+
+    # The algorithm executes until we visit all nodes
+    while unvisited_nodes:
+        # The code block below finds the node with the lowest score
+        current_min_node = None
+        for node in unvisited_nodes:  # Iterate over the nodes
+            if current_min_node == None:
+                current_min_node = node
+            elif shortest_path[node] < shortest_path[current_min_node]:
+                current_min_node = node
+
+        # The code block below retrieves the current node's neighbors and updates their distances
+        neighbors = graph.out_edges(current_min_node)
+        for neighbor in neighbors:
+            tentative_value = shortest_path[current_min_node] + graph.get_edge_data(current_min_node, neighbor)['weight']
+            if tentative_value < shortest_path[neighbor]:
+                shortest_path[neighbor] = tentative_value
+                # We also update the best path to the current node
+                previous_nodes[neighbor] = current_min_node
+
+        # After visiting its neighbors, we mark the node as "visited"
+        unvisited_nodes.remove(current_min_node)
+
+    return previous_nodes, shortest_path
 
 # Format the output
 def desired_result_string(flight_number, SourceAirport, SourceAirport_Country, DestinationAirport,
@@ -49,3 +90,12 @@ def desired_result_string(flight_number, SourceAirport, SourceAirport_Country, D
     Time: {FlyTime}h
     Price: {Price}$
     ----------------------------'''
+
+
+add_nodes_to_DiWeGraph()
+add_edge(2)
+print(G.nodes.get(1))
+first_node = next(iter(G.nodes(data=True)))
+print(first_node)
+print(type(G.get_edge_data(G.nodes.get(0),G.nodes.get(1))['weight']))
+print(dijkstra_algorithm(G,G.nodes.get(0)))
