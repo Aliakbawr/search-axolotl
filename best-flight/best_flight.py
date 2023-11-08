cd8275edf87dba6a4bd1bfbf950c51a91d9e49d6
Aliakbar Ahrari <ahr04624@gmail.com>
2023-11-08 11:49:43 +0330

    Update best_flight.py

--- best-flight/best_flight.py
+++ best-flight/best_flight.py
@@ -23,37 +23,24 @@ def add_nodes_to_DiWeGraph():
         G.add_node(uniqueAirport.iloc[i], heuristic=0)
 
 
-def a_star_generated_cost(param):
-    distance = param['Distance']
-    time = param['FlyTime']
-    price = param['Price']
-
-    return 3*distance + 100*time + 20*price
-
-
-def dijkstra_generated_cost(param):
+def generated_cost(param):
     distance = param['Distance']
     price = param['Price']
     time = param['FlyTime']
-    w2 = 0.3  # Weight for distance
-    w3 = 0.3  # Weight for price
-    w1 = 0.4  # Weight for time
+    w2 = 3  # Weight for distance
+    w3 = 20  # Weight for price
+    w1 = 100  # Weight for time
 
     cost = w1 * time + w2 * distance + w3 * price
     return cost
 
 
 # Two options for creating edges using costs
-def add_edge(i):
-    if i == 1:
-        for i in range(12850):
-            cost = a_star_generated_cost(df.iloc[i])
-            G.add_edge(df.iloc[i, 1], df.iloc[i, 2], weight=cost, distance='Distance', time='FlyTime', price='Price')
-    else:
-        for i in range(12849):
-            cost = dijkstra_generated_cost(df.iloc[i])
-            G.add_edge(df.iloc[i, 1], df.iloc[i, 2], weight=cost,
-                       Distance=df.iloc[i, 13], FlyTime=df.iloc[i, 14], Price=df.iloc[i, 15])
+def add_edge():
+    for i in range(12849):
+        cost = generated_cost(df.iloc[i])
+        G.add_edge(df.iloc[i, 1], df.iloc[i, 2], weight=cost,
+                   Distance=df.iloc[i, 13], FlyTime=df.iloc[i, 14], Price=df.iloc[i, 15])
 
 
 # Dijkstra Implementation
@@ -79,7 +66,7 @@ def desired_result_string(path):
         total_time += time
         total_price += price
         total_distance += distance
-        if shortest_path is not None:
+        if path is not None:
             result_string += f'''
             Flight #{flight_number}: 
                 From: {u}
@@ -99,19 +86,6 @@ def desired_result_string(path):
     return result_string  # Return the result string
 
 
-print('Enter Airports:')
-# airport_1 = input()
-# airport_2 = input()
-add_nodes_to_DiWeGraph()
-add_edge(2)
-
-first_node = next(iter(G.nodes(data=True)))
-airport_1 = 'Imam Khomeini International Airport'
-airport_2 = 'John F Kennedy International Airport'
-
-# Testing Dijkstra string output
-shortest_path = dijkstra_shortest_path(airport_1, airport_2)
-print(desired_result_string(shortest_path))
 # Format the output
 def desired_result_string(flight_number, SourceAirport, SourceAirport_Country, DestinationAirport,
                           DestinationAirport_Country, Distance, FlyTime, Price):
@@ -134,7 +108,7 @@ def calculate_distance(lat1, lon1, lat2, lon2):
     # Haversine formula
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
     # Radius of earth in kilometers. Use 3956 for miles
@@ -160,17 +134,17 @@ def a_star_heuristic(DestinationAirport):
                 soLatitude = df.iloc[i, 5]
                 soLongitude = df.iloc[i, 6]
                 distance = calculate_distance(soLatitude, soLongitude, desLatitude, desLongitude)
-                G.nodes[df.iloc[i,1]]['heuristic'] = 67043*distance
+                G.nodes[df.iloc[i, 1]]['heuristic'] = 67043 * distance
                 break
             elif df.iloc[i, 2] == node[1]:
                 soLatitude = df.iloc[i, 10]
                 soLongitude = df.iloc[i, 11]
                 distance = calculate_distance(soLatitude, soLongitude, desLatitude, desLongitude)
-                G.nodes[df.iloc[i,2]]['heuristic'] = 67043*distance
+                G.nodes[df.iloc[i, 2]]['heuristic'] = 67043 * distance
                 break
 
 
-def a_star_algorithm(SourceAirport,DestinationAirport):
+def a_star_algorithm(SourceAirport, DestinationAirport):
     a_star_heuristic(DestinationAirport)
     queue = [(0, SourceAirport)]
     visited = set()
