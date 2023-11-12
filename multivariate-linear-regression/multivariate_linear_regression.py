5720c071433653fc824fe5a14ff0197cb9313631
Aliakbar Ahrari <ahr04624@gmail.com>
2023-11-12 14:38:43 +0330

    Updated added files

--- multivariate-linear-regression/multivariate_linear_regression.py
+++ multivariate-linear-regression/multivariate_linear_regression.py
@@ -1,7 +1,28 @@
 import pandas as pd
 
 #Handling csv file
-file_name = './multivariate-linear-regression/Flight_Price_Dataset_Q2.csv'
+file_name = 'Flight_Price_Dataset_Q2.csv'
 df = pd.read_csv(file_name)
-#print(df.head())
+df_1 = df.drop_duplicates(subset=['departure_time'])
+print(df_1)
+print('================================================')
+df_2 = df.drop_duplicates(subset=['stops'])
+print(df_2)
+print('================================================')
+df_3 = df.drop_duplicates(subset=['arrival_time'])
+print(df_3)
+print('================================================')
+df_4 = df.drop_duplicates(subset=['class'])
+print(df_4)
+print('================================================')
+df_5 = df.drop_duplicates(subset=['duration'])
+print(df_5)
+print('================================================')
+df_6 = df.drop_duplicates(subset=['days_left'])
+print(df_6)
+print('================================================')
+df_7 = df.drop_duplicates(subset=['price'])
+print(df_7)
+print('================================================')
+
 
