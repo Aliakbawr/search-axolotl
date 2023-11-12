56d4fa0f8bc6b4cf1e20a5c36ad14220fd0c37ea
MehrAzin Marzough <mehrazin.marzough@gmail.com>
2023-11-12 15:35:49 +0330

    mapped data frame creation

--- multivariate-linear-regression/multivariate_linear_regression.py
+++ multivariate-linear-regression/multivariate_linear_regression.py
@@ -1,7 +1,22 @@
 import pandas as pd
 
-#Handling csv file
-file_name = './multivariate-linear-regression/Flight_Price_Dataset_Q2.csv'
-df = pd.read_csv(file_name)
-#print(df.head())
 
+def df_creator():
+    file_name = './Flight_Price_Dataset_Q2.csv'
+    data_frame = pd.read_csv(file_name)
+    mapping = {'zero': 3,
+               'one': 2,
+               'two_or_more': 1}
+
+    data_frame['stops_mapping'] = data_frame['stops'].map(mapping)
+    mapping = {'Economy': 1,
+               'Business': 2}
+    data_frame['class_mapping'] = data_frame['class'].map(mapping)
+
+    departure_dummies = pd.get_dummies(data_frame['departure_time'])
+    arrival_dummies = pd.get_dummies(data_frame['arrival_time'])
+    encoded_df = pd.concat([data_frame, departure_dummies, arrival_dummies], axis=1)
+    return encoded_df
+
+
+df = df_creator()
